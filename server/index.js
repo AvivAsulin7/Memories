f579c989d729aa53d57632f61a741f3462d1e318
אביב אסולין <avivasulin1211@gmail.com>
2022-10-02 11:31:35 +0300

    fixing erros in server

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes); //every route from posts.js will start in /posts
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes); //every route from posts.js will start in /posts
 
 const CONNECTION_URL =
   "mongodb+srv://Avivasulin1211:Aviv852456@cluster0.fzixazu.mongodb.net/?retryWrites=true&w=majority";
