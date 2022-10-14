2ed212ef56644674fc3d9aa6df9ae8be261c176a
אביב אסולין <avivasulin1211@gmail.com>
2022-10-14 11:51:38 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -15,12 +15,12 @@ app.use(cors());
 
 app.use("/posts", postRoutes); //every route from posts.js will start in /posts
 
-// const CONNECTION_URL =
-//   "mongodb+srv://Avivasulin1211:Aviv852456@cluster0.fzixazu.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL =
+  "mongodb+srv://Avivasulin1211:Aviv852456@cluster0.fzixazu.mongodb.net/?retryWrites=true&w=majority";
+const PORT = 5000 || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, {
+  .connect(CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
